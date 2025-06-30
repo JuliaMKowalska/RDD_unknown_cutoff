0eec8d88ba633a94b28e1000034dac1de36bafef
JuliaMKowalska <julia.kowalska@interia.com>
2025-06-30 11:42:51 +0200

    Update Simulations2024_main.R

--- Simulations2024_replication/Simulations2024_main.R
+++ Simulations2024_replication/Simulations2024_main.R
@@ -42,8 +42,9 @@ source('Simulations2024_functions.R')
 # sign - correct sign identification of robust 95% confidence interval,
 # j_abs - absolute error of the estimate of the compliance rate
 
-
-
+#----------------------------#
+#  Section 5 & Appendix B.1  #
+#----------------------------#
 
 ## Fuzzy design ##
 # A single simulation takes around 4 minutes. In the following code we split simulations
@@ -420,4 +421,293 @@ lineF.df=data.frame(X=x,Y=Fun)
 
 
 ggarrange(pllee,plludwig,ncol=2)
+
+
+#---------------------------#
+#  Section 5 & Appendix B.2 #
+#---------------------------#
+
+# In the following simulations the outcome functions do not matter for the cutoff detection as only treatment models are used
+# However, for each outcome function different seeds are used, therefore for the sake of precision we repeat cutoff detection
+# with the corresponding seeds.
+
+# In the LoTTA treatment-only model one simulations takes around 45 seconds. In the simple model with two constant functions one simulation takes around 7 seconds. 
+
+
+# Function A 
+
+# LoTTA treatement-only model
+
+  # jump = 0.55 
+  
+  system.time(postA_1to500<-lapply(2001:2500, simulation_treatment,"0.55"))
+  system.time(postA_501to1000<-lapply(2501:3000, simulation_treatment,"0.55"))
+    
+    # Results for the cutoff detection
+
+  Results1<-lapply(postA_1to500, performance_cutoff_sample_FUZZY)
+  Results2<-lapply(postA_501to1000, performance_cutoff_sample_FUZZY)
+  Results1=do.call(rbind.data.frame, Results1)
+  Results2=do.call(rbind.data.frame, Results2)
+  Results=rbind(Results1,Results2) 
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"A","0.55")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+  # jump = 0.30
+
+  system.time(postA_1to500<-lapply(2001:1500, simulation_treatment,"0.3"))
+  system.time(postA_501to1000<-lapply(2501:3000, simulation_treatment,"0.3"))
+    
+    # Results for the cutoff detection
+
+  Results1<-lapply(postA_1to500, performance_cutoff_sample_FUZZY)
+  Results2<-lapply(postA_501to1000, performance_cutoff_sample_FUZZY)
+  Results1=do.call(rbind.data.frame, Results1)
+  Results2=do.call(rbind.data.frame, Results2)
+  Results=rbind(Results1,Results2) 
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"A","0.3")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+
+# Two constant functions
+  
+  # jump = 0.55 
+  
+  system.time(postA_1to1000_s<-lapply(2001:3000, simulation_treatment_simple,"0.55"))
+    
+    # Results for the cutoff detection (not in the manuscript)
+
+  Results<-lapply(postA_1to1000_s, performance_cutoff_sample_FUZZY)
+  Results=do.call(rbind.data.frame, Results)
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"A","0.55")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+  # jump = 0.30
+
+  system.time(postA_1to1000_s<-lapply(2001:3000, simulation_treatment_simple,"0.3"))
+    
+    # Results for the cutoff detection (not in the manuscript)
+
+  Results<-lapply(postA_1to1000_s, performance_cutoff_sample_FUZZY)
+  Results=do.call(rbind.data.frame, Results)
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"A","0.3")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+
+
+# Function B 
+
+# LoTTA treatement-only model
+
+  # jump = 0.55 
+  
+  system.time(postB_1to500<-lapply(1:500, simulation_treatment,"0.55"))
+  system.time(postB_501to1000<-lapply(501:1000, simulation_treatment,"0.55"))
+    
+    # Results for the cutoff detection
+
+  Results1<-lapply(postB_1to500, performance_cutoff_sample_FUZZY)
+  Results2<-lapply(postB_501to1000, performance_cutoff_sample_FUZZY)
+  Results1=do.call(rbind.data.frame, Results1)
+  Results2=do.call(rbind.data.frame, Results2)
+  Results=rbind(Results1,Results2) 
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"B","0.55")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+  # jump = 0.30
+
+  system.time(postB_1to500<-lapply(1:500, simulation_treatment,"0.3"))
+  system.time(postB_501to1000<-lapply(501:1000, simulation_treatment,"0.3"))
+    
+    # Results for the cutoff detection
+
+  Results1<-lapply(postB_1to500, performance_cutoff_sample_FUZZY)
+  Results2<-lapply(postB_501to1000, performance_cutoff_sample_FUZZY)
+  Results1=do.call(rbind.data.frame, Results1)
+  Results2=do.call(rbind.data.frame, Results2)
+  Results=rbind(Results1,Results2) 
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"B","0.3")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+
+# Two constant functions
+  
+  # jump = 0.55 
+  
+  system.time(postB_1to1000_s<-lapply(1:1000, simulation_treatment_simple,"0.55"))
+    
+    # Results for the cutoff detection (not in the manuscript)
+
+  Results<-lapply(postB_1to1000_s, performance_cutoff_sample_FUZZY)
+  Results=do.call(rbind.data.frame, Results)
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"B","0.55")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+  # jump = 0.30
+
+  system.time(postB_1to1000_s<-lapply(1:1000, simulation_treatment_simple,"0.3"))
+    
+    # Results for the cutoff detection (not in the manuscript)
+
+  Results<-lapply(postB_1to1000_s, performance_cutoff_sample_FUZZY)
+  Results=do.call(rbind.data.frame, Results)
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"B","0.3")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+# Function C 
+
+# LoTTA treatement-only model
+
+  # jump = 0.55 
+  
+  system.time(postC_1to500<-lapply(1001:1500, simulation_treatment,"0.55"))
+  system.time(postC_501to1000<-lapply(1501:2000, simulation_treatment,"0.55"))
+    
+    # Results for the cutoff detection
+
+  Results1<-lapply(postC_1to500, performance_cutoff_sample_FUZZY)
+  Results2<-lapply(postC_501to1000, performance_cutoff_sample_FUZZY)
+  Results1=do.call(rbind.data.frame, Results1)
+  Results2=do.call(rbind.data.frame, Results2)
+  Results=rbind(Results1,Results2) 
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"C","0.55")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+  # jump = 0.30
+
+  system.time(postC_1to500<-lapply(1001:1500, simulation_treatment,"0.3"))
+  system.time(postC_501to1000<-lapply(1501:2000, simulation_treatment,"0.3"))
+    
+    # Results for the cutoff detection
+
+  Results1<-lapply(postC_1to500, performance_cutoff_sample_FUZZY)
+  Results2<-lapply(postC_501to1000, performance_cutoff_sample_FUZZY)
+  Results1=do.call(rbind.data.frame, Results1)
+  Results2=do.call(rbind.data.frame, Results2)
+  Results=rbind(Results1,Results2) 
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"C","0.3")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+
+# Two constant functions
+  
+  # jump = 0.55 
+  
+  system.time(postC_1to1000_s<-lapply(1001:2000, simulation_treatment_simple,"0.55"))
+    
+    # Results for the cutoff detection (not in the manuscript)
+
+  Results<-lapply(postC_1to1000_s, performance_cutoff_sample_FUZZY)
+  Results=do.call(rbind.data.frame, Results)
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"C","0.55")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+  # jump = 0.30
+
+  system.time(postC_1to1000_s<-lapply(2001:3000, simulation_treatment_simple,"0.3"))
+    
+    # Results for the cutoff detection (not in the manuscript)
+
+  Results<-lapply(postC_1to1000_s, performance_cutoff_sample_FUZZY)
+  Results=do.call(rbind.data.frame, Results)
+  apply(Results, 2,mean)
+  apply(Results, 2,median)
+  sqrt(mean(Results[['abs_err_map']]^2)) 
+    
+    # Results for the treatment effect estimation with the plug-in estimator
+  
+  Results_plugin<-lapply(1:1000, plugin_estimator,Results,"C","0.3")
+  Results_plugin=do.call(rbind.data.frame, Results_plugin)
+  apply(Results_plugin, 2,mean)
+  sqrt(mean(Results_plugin[['abs_err']]^2))
+
+
 
